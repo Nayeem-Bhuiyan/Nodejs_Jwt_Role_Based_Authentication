29e65e790d6d6684f5c4e9f1388ea525dd84e817
Nayeem-Bhuiyan <nayeembhuiyan40@gmail.com>
2022-08-17 11:37:53 +0600

    appjsedit

--- project/app.js
+++ project/app.js
@@ -1,11 +1,17 @@
+//built npm module imports
 const express = require('express')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const path = require('path');
-const _config = require('../project/app/helper/config');
+const app = express();
 
-const app = express()
+//built custom module imports
+const _config = require('../project/app/helper/config');
+const _userRouter=require('./app/router/userRouter');
+const _authrouter=require('./app/router/authrouter')
+
+
 
 app.set('port', _config.serverPort || 5000)
 
@@ -17,8 +23,8 @@ app.use(cookieParser())
 app.use(express.json({ limit: '5000mb' }));
 app.use(express.static(path.join(__dirname+'/uploads')));
 app.use(express.urlencoded())
-app.use('/auth', require('./app/router/authrouter'))
-app.use('/users', require('./app/router/userRouter'))
+app.use('/auth', _authrouter)
+app.use('/users',_userRouter)
 
 app.listen(app.get('port'), () =>
   console.log(`http://localhost:${app.get('port')}`)
